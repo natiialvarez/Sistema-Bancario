a7ac93c617d7f5416d490fcd4753a29143679f6d
Natalia Alvarez <natalia.fsalvarez@gmail.com>
2023-08-23 13:24:38 -0300

    adicionado uma mensagem para caso não haja nenhuma conta

--- src/controladores/contas.js
+++ src/controladores/contas.js
@@ -1,6 +1,9 @@
 const { contas } = require("../bancodedados")
 
 const listarContas = (req, res) => {
+    if (contas.length === 0) {
+        return res.status(200).json({ mensagem: "Nenhuma conta existente.", contas })
+    }
     return res.status(200).json(contas)
 }
 
